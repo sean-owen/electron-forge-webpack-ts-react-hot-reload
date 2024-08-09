2aa6cefae0e12e4eb41052986d25d83ba282f120
Sean Owen <sean.o@telematic.com>
2024-08-09 15:19:22 -0600

    fix root render to match todays standards

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,9 @@
 import { createRoot } from 'react-dom/client';
 
-const root = createRoot(document.body);
-root.render(<h2>Hello from React! another what a weird bug</h2>);
+// const root = createRoot(document.body);
+// root.render(<h2>Hello from React! another what a weird bug</h2>);
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+    <h2>Hello from React! test sean</h2>
+);
